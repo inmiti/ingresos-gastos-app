03e903c7996650d336d374b6f0f92242f1fe5752
inmiti <inmabauvel@gmail.com>
2022-12-21 21:14:44 +0100

    iteranndo con jinja

--- app_registro/routes.py
+++ app_registro/routes.py
@@ -3,8 +3,9 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    datos = [{'fecha':'18/12/2022','concepto': 'Regalo','cantidad':-276.00}, { 'fecha':'19/12/2022','concepto': 'Nomina', 'cantidad':1700}, {'fecha':'20/12/2022','concepto': 'Ropa','cantidad':-56.50} ]
+    return render_template("index.html", pageTitle = "Listas", lista = datos)
 
 @app.route("/new")
 def create():
-    return render_template("new.html")
+    return render_template("new.html", pageTitle = "Nuevo")
